226ec78df1e44779a05bbc3e717efddf83087dfd
Nikhil Varma Pandeti <nikhil.pandeti@gmail.com>
2017-02-09 18:43:36 -0600

    Only black dots
    2/9/17

--- viPro.py
+++ viPro.py
@@ -3,34 +3,39 @@ import numpy as np
 
 image = cv2.imread("di_in_cache.jpeg")
 
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-gray = cv2.bilateralFilter(gray, 11, 17, 17)
-edged = cv2.Canny(gray, 30, 200)
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray_image = cv2.bilateralFilter(gray_image, 11, 17, 17)
+edged = cv2.Canny(gray_image, 30, 200)
 
-_, cnts, _ = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
+_, cnts, _ = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) 
+cnts = sorted(cnts, key= cv2.contourArea, reverse = True)[:5]
 candidates = []
 
 for c in cnts:
     peri = cv2.arcLength(c, True)
     approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-    if len(approx) > 5:
+    if len(approx) > 2:
         candidates.append(approx)
-
-mask = np.zeros_like(image)
+        
+mask = np.zeros_like(gray_image)
 cv2.drawContours(mask, candidates, 0, (255, 255, 255), -1)
 
+cv2.imshow("mask", mask)
 
-out = np.zeros_like(image)
-out[mask == 255] = image[mask == 255]
+out = np.zeros_like(gray_image)
+out[mask == 255] = gray_image[mask == 255]
 
-cv2.drawContours(image, candidates, -1, (0, 255, 0), 3)
-
-ret, th = cv2.threshold(out, 80, 255, cv2.THRESH_BINARY)
+cv2.imshow("woo", out)
+ret,filter_image = cv2.threshold(gray_image, 150, 255, cv2.THRESH_BINARY)
+ret,filter_maskimage = cv2.threshold(out, 150, 255, cv2.THRESH_BINARY)
 
 try:
     cv2.imshow("mask", mask)
     cv2.imshow("woo", out)
+    cv2.imshow("Dice", image)
+    cv2.imshow("Dice - gray", gray_image)
+    cv2.imshow("Dice - filtered", filter_image)
+    cv2.imshow("Dice - masked - filtered", filter_maskimage)
     cv2.waitKey(0)
 except:
     from matplotlib import pyplot as plt
