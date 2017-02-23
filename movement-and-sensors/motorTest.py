d3647daee19c7a545c49eacdce3e02873bf1302a
root <root@x.massivesoft.net>
2017-02-23 12:03:57 +0100

    adding basic motor test script

---
+++ movement-and-sensors/motorTest.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+# This is a very basic test to make the DC motors plugged into the L293D chip move forwards and backwards at variable speeds
+# the pi currently cannot interface with the encoders due to a lack of GPIO ports (extender needed, or we can use reserved pins)
+
+import sys
+import time
+import RPi.GPIO as io
+import wiringpi2 as wiringpi #apt-get install wiringpi
+io.setmode(io.BCM)
+wiringpi.wiringPiSetupGpio()
+wiringpi.pinMode(18,2)
+wiringpi.pwmWrite(18, 0)
+
+motors[0] = 5 # motor 1
+motors[1] = 6
+motors[2] = 13 # motor 2
+motors[3] = 19
+motors[4] = 26 # motor 3
+motors[5] = 21
+motors[6] = 20 # motor 4
+motors[7] = 16
+
+def motor(number):
+	return number-2+number;
+
+for motor in motors:
+	io.setup(motor, io.OUT)
+
+def speed(speed):
+	if 0 <= speed <= 1024:
+		wiringpi.pwmWrite(18, speed)
+	else:
+		wiringpi.pwmWrite(18, 0)
+
+def clockwise(motor_number, speed):
+	speed(speed)
+	io.output(motors[motor_number], True)
+	io.output(motors[motor_number+1], False)
+
+def counter_clockwise(motor_number, speed):
+	speed(speed)
+	io.output(motors[motor_number], False)
+	io.output(motors[motor_number+1], True)
+
+clockwise(motor(4), 512) # etc...
