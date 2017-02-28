0cca6efdb3f4ebc1286fe446ae785b2083b27bb6
Sebastian King <sebastian-king@users.noreply.github.com>
2017-02-28 14:49:40 -0600

    Update install-a-pi.sh

--- install-a-pi.sh
+++ install-a-pi.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
+echo "THE PI WILL REBOOT ONCE THIS SCRIPT HAS FINISHED RUNNING!";
+read -r -p "Press X to exit the script now if you do not want to reboot " response;
+if [[ "$response" =~ ^([xX])+$ ]]; then
+    exit;
+fi
+
+raspi-config --expand-rootfs
+
 apt-get update
 apt-get upgrade
 apt-get dist-upgrade
 
-apt-get install git
-cd /usr/src/
+apt-get -y install git
 
+cd ~
 git clone https://github.com/afloresescarcega/UTR5.git # perhaps ignore this file
 
-adduser r5 --home /home/r5 -q --disabled-password --gecos GECOS
+#adduser r5 --home /home/r5 -q --disabled-password --gecos GECOS
 
 #su r5
 #cd ~
 
-sudo apt-get install python python-pip python-dev python3 python-pip3 python3-dev
+sudo apt-get -y install python python-pip python-dev python3 python-pip3 python3-dev
 pip install wiringpi
 pip install wiringpi2
+
+sudo reboot
