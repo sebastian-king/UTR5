78529e1d44f5f19dd9d71d9e1ae2552eef99cf24
cole-thompson <colethompson@utexas.edu>
2017-02-28 19:45:07 -0600

    strafe_to_block, typeos

--- movement-and-sensors/tests/movement-wrapper.py
+++ movement-and-sensors/tests/movement-wrapper.py
@@ -47,7 +47,7 @@ def move(distance, direction):
 
 def turn(degrees);
 	if degrees != 0:
-		current_direction = current_direction + degrees	
+		map-data.setDir(map-data.getDir() + degrees)	
 		if degrees > 0:
 			dir = 0
 			angle = degrees
@@ -72,8 +72,8 @@ blocklength = 10     #constant that we need to measure
 
 #strafe one block no turning. direction: 0=right, 1=fwd, 2=left, 3=back
 def strafe_one_block(direction):
-	x = map_data.getX
-	y = map_data.getY
+	x = map-data.getX
+	y = map-data.getY
 	if direction == 0:
 		x = x + 1
 	elif direction == 1:
@@ -84,8 +84,8 @@ def strafe_one_block(direction):
 		y = y - 1
 
 	if is_valid_loc(x, y):
-		map_data.setX(x)
-		map_data.setX(y)
+		map-data.setX(x)
+		map-data.setX(y)
 		move(blocklength, direction)
 	#else: error in movement algorithm
 
@@ -109,7 +109,7 @@ def move_to_block(x, y):
 			turn_angle = turn_angle * (-1/3)
 		turn(turn_angle)
 		move(distance, 1)
-		map_data.setX(x)
+		map-data.setX(x)
 
 		//y movement
 		turn_angle = 0
@@ -124,7 +124,35 @@ def move_to_block(x, y):
 			turn_angle = turn_angle * (-1/3)
 		turn(turn_angle)
 		move(distance, 1)
-		map_data.setX(y)
+		map-data.setY(y)
+
+def strafe_to_block(x, y):
+	if map_data.is_valid_loc(x, y):
+		x_diff = x - map_data.getLoc[0]
+		y_diff = y - map_data.getLoc[1]
+		
+		//x movement
+		distance = 0;
+		dir = 0
+		if x_diff > 0:
+			distance = blocklength * x_diff
+			dir = 0
+		elif x_diff < 0:
+			distance = blocklength * x_diff * -1)
+			dir = 2
+		move(distance, dir)
+		map-data.setX(x)
+
+		//y movement
+		distance = 0;
+		if y_diff > 0:
+			dir = 1
+			distance = blocklength * y_diff
+		elif y_diff < 0:
+			dir = 3
+			distance = blocklength * y_diff * -1)
+		move(distance, dir)
+		map-data.setY(y)
 		
 			
 
