97e9d5e11c7b648ceec9d2c3c320c9aa0ba287e9
Florian <jaflo@users.noreply.github.com>
2017-03-02 19:04:11 -0600

    Use adaptive thresholding

--- visual-processing/viPro.py
+++ visual-processing/viPro.py
@@ -26,8 +26,8 @@ out = np.zeros_like(gray_image)
 out[mask == 255] = gray_image[mask == 255]
 
 #cv2.imshow("woo", out)
-ret,filter_image = cv2.threshold(gray_image, 150, 255, cv2.THRESH_BINARY)
-ret,filter_maskimage = cv2.threshold(out, 150, 255, cv2.THRESH_BINARY)
+ret,filter_image = cv2.threshold(gray_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+ret,filter_maskimage = cv2.threshold(out, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
 dots = cv2.bilateralFilter(filter_maskimage, 11, 17, 17)
 _, cnts, _ = cv2.findContours(dots, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) 
