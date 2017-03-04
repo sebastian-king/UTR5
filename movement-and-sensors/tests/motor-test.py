97939dcf241c89e81100afd2f5d135459a328a10
Sebastian King <sebastian-king@users.noreply.github.com>
2017-03-03 21:36:58 -0600

    committed master-ready version

--- movement-and-sensors/tests/motor-test.py
+++ movement-and-sensors/tests/motor-test.py
@@ -1,20 +1,15 @@
 import RPi.GPIO as GPIO
 import time
-import wiringpi
 
 GPIO.setmode(GPIO.BCM)
 
-#wiringpi.wiringPiSetupGpio()
-#wiringpi.pinMode(18, 2)
-#wiringpi.pwmWrite(18, 0)
+GPIO.setup(22,GPIO.OUT) # motor 1
+GPIO.setup(27,GPIO.OUT)
+GPIO.setup(19,GPIO.OUT) # motor 2
+GPIO.setup(26,GPIO.OUT)
 
-GPIO.setup(22,GPIO.OUT) # attached to pin 9
-GPIO.setup(27,GPIO.OUT) # attached to pin 9
-GPIO.setup(19,GPIO.OUT) # attached to pin 9
-GPIO.setup(26,GPIO.OUT) # attacbed to pin 28
-
-GPIO.setup(17,GPIO.OUT) # attacbed to pin 28
-GPIO.setup(18,GPIO.OUT) # attacbed to pin 28
+GPIO.setup(17,GPIO.OUT) # PWM for motor 1
+GPIO.setup(18,GPIO.OUT) # PWM for motor 2
 
 p1 = GPIO.PWM(17, 1024)
 p2 = GPIO.PWM(18, 1024)
@@ -22,44 +17,31 @@ p2 = GPIO.PWM(18, 1024)
 p1.start(40)
 p2.start(40)
 
-print "1"
-#wiringpi.pwmWrite(18, 400)
+print "MOTOR 1 CW"
 GPIO.output(19,GPIO.LOW)
 GPIO.output(26,GPIO.HIGH)
 time.sleep(1)
-print "2"
-#wiringpi.pwmWrite(18, 512)
+print "MOTOR 1 CCW"
 GPIO.output(19,GPIO.HIGH)
 GPIO.output(26,GPIO.LOW)
 time.sleep(1)
-print "3"
-
+print "MOTOR 1 STOP"
 GPIO.output(19,GPIO.LOW)
 GPIO.output(26,GPIO.LOW)
-
-#wiringpi.pwmWrite(18, 0)
 time.sleep(2)
 
-print "1"
+print "MOTOR 2 CW"
 GPIO.output(22,GPIO.LOW)
 GPIO.output(27,GPIO.HIGH)
-time.sleep(2)
-print "2"
+time.sleep(1)
+print "MOTOR 2 CCW"
 GPIO.output(22,GPIO.HIGH)
 GPIO.output(27,GPIO.LOW)
-time.sleep(2)
-print "3"
-
+time.sleep(1)
+print "MOTOR 2 STOP"
 GPIO.output(22,GPIO.LOW)
 GPIO.output(27,GPIO.LOW)
-
-#for a in range(10):
-#	print "PWM: " + str(a*100)
-#	#wiringpi.pwmWrite(18, a*100)
-#	p.ChangeDutyCycle(a*10)
-#	time.sleep(0.2)
 
 p1.ChangeDutyCycle(0)
 p2.ChangeDutyCycle(0)
-#wiringpi.pwmWrite(18, 0)
 GPIO.cleanup()
