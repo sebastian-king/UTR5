726412e657684cb8a984b724d5f73bac28ae90d2
Sebastian King <sebastian-king@users.noreply.github.com>
2017-03-03 21:32:06 -0600

    created program for controlling the underglow

--- leds/leds.py
+++ leds/leds.py
@@ -4,7 +4,7 @@ import RPi.GPIO as GPIO
 GPIO.setwarnings(False)
 
 # echo 11 > /sys/class/gpio/unexport
-ON = int(sys.argv[1]) # 1 = ON, 0 = OFF
+ON = int(sys.argv[1]) # 1 = ON, 0 = OFF; `chmod +x leds.py && ./leds.py 1`
 
 GPIO.setmode(GPIO.BCM)
 
