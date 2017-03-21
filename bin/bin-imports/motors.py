cb09dbdc821b63863e9fe1896a9919f67714bb38
cole-thompson <colethompson@utexas.edu>
2017-03-20 20:05:42 -0500

    started motor.py that will work with encoder.py

---
+++ bin/bin-imports/motors.py
@@ -0,0 +1,21 @@
+
+#encoder.py has encoder class that takes 2 pins for quadrature encoders
+import encoder
+
+motorEncoders = [0 for a in range(4)]
+
+def initMotors(void):
+
+
+def encoderHandler(void):
+    movementEncoder.monitor();
+
+#for any GPIO initialization
+def initializeGPIO(void):    
+    
+    #set up encoder interrupt, call encoderHandler if either pins change
+    GPIO.add_event_detect(encoderPinA, GPIO.BOTH, callback = encoderHandler)
+    GPIO.add_event_detect(encoderPinB, GPIO.BOTH, callback = encoderHandler)
+
+def run_all_motors(speed, pulses, dir1, dir2, dir3, dir4):
+    number = 0
