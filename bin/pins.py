0a5573c984adaf79a58e25267da4611f7100b047
cSchubes <carson.schubert14@gmail.com>
2017-03-20 19:58:53 -0500

    Added pin mapping file

---
+++ bin/pins.py
@@ -0,0 +1,30 @@
+#a class for storing all wiring information and pin numbers
+
+#Left Front Motor Pins
+leftFrontMotorPWM = 0
+leftFrontMotorEnableA = 0
+leftFrontMotorEnableB = 0
+leftFrontEncoderChA = 0
+leftFrontEncoderChB =0
+
+#Right Front Motor Pins
+rightFrontMotorPWM = 0
+rightFrontMotorEnableA = 0
+rightFrontMotorEnableB = 0
+rightFrontEncoderChA = 0
+rightFrontEncoderChB = 0
+
+#Left Rear Motor Pins
+leftRearMotorPWM = 0
+leftRearMotorEnableA = 0
+leftRearMotorEnableB = 0
+leftRearEncoderChA = 0
+leftRearEncoderChB = 0
+
+#Right Rear Motor Pins
+rightRearMotorPWM = 0
+rightRearMotorEnableA = 0
+rightRearMotorEnableB = 0
+rightRearEncoderChA = 0
+rightRearEncoderChB = 0
+
