936b7a0dc2b7d0e0c1e97e37a2217f36fde7c133
jaflo <imjaflo@gmail.com>
2017-03-30 02:40:48 -0500

    Start and stop button pins updated

--- buttons/start_button.py
+++ buttons/start_button.py
@@ -10,26 +10,27 @@ import pins
 def listen_for_start():
 	GPIO.setwarnings(False)
 	GPIO.setmode(GPIO.BCM)
-	GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-	GPIO.setup(12, GPIO.OUT)
-	GPIO.output(12, GPIO.LOW)
+	GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+	#GPIO.setup(12, GPIO.OUT)
+	#GPIO.output(12, GPIO.LOW)
 
 	# we should always have the bottom left lit yellow
-	GPIO.output(pins.matrix[8], GPIO.HIGH)
-	GPIO.output(pins.matrix[29], GPIO.HIGH)
-	GPIO.output(pins.matrix[31], GPIO.HIGH) # 31 = rowpins[6]
+	#GPIO.output(pins.matrix[8], GPIO.HIGH)
+	#GPIO.output(pins.matrix[29], GPIO.HIGH)
+	#GPIO.output(pins.matrix[31], GPIO.HIGH) # 31 = rowpins[6]
 
 	#i=0;
 	while True: # this waits for the start button to be pressed
 		#i=i+1
 		time.sleep(0.1)
-		val = GPIO.input(22)
+		val = GPIO.input(18)
 		#print str(i) + " " + str(val)
 		if val == 0:
 			break
 
+	import displays
 	print "GO"
-	main.main()
+#	main.main()
 
 	"""GPIO.cleanup()           # clean up GPIO on normal exit
 
--- buttons/stop_button.py
+++ buttons/stop_button.py
@@ -2,26 +2,26 @@ import RPi.GPIO as GPIO
 import sys, os, time
 
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../bin/bin-imports")
-import displays
+#import displays
 
 def listen_for_stop():
 	GPIO.setwarnings(False)
 	GPIO.setmode(GPIO.BCM)
-	GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-	GPIO.setup(26, GPIO.OUT)
-	GPIO.output(26, GPIO.LOW)
+	GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 	#i=0;
 	while True: # wait for the stop button to be pressed
 		#i=i+1
 		time.sleep(0.1)
-		val = GPIO.input(5)
+		val = GPIO.input(23)
 		#print str(i) + " " + str(val)
 		if val == 0:
 			break
 
 	print "Stop pressed"
-	displays.show()
+	#displays.show()
+	import displays
+	displays.showNumber(4)
 
 	"""GPIO.cleanup()           # clean up GPIO on normal exit
 
