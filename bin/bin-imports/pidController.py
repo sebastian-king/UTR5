2f7bc9f3839fdc30a04c951f7a93998a17deef69
cSchubes <carson.schubert14@gmail.com>
2017-03-30 21:56:09 -0500

    pid

--- bin/bin-imports/pidController.py
+++ bin/bin-imports/pidController.py
@@ -50,7 +50,7 @@ class pidController:
             self.previousError = error
             
             out = P_value + (self.Integral*self.Ki)#+ (derivative*self.Kd) //currently not using derivative
-            self.output = max(0, min((((self.setPoint/120.0)*1000.0) + out),1000))
+            self.output = max(0, min((((self.setPoint/127.0)*1000.0) + out),1000))
               
         
     def getOutput(self):
