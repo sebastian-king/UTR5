cc38dfb6d0550bc332e5b888715950efed57ac6b
smt2868 <seantremblay@utexas.edu>
2017-03-31 02:59:16 -0500

    Update Servo.py

--- bin/bin-imports/Servo.py
+++ bin/bin-imports/Servo.py
@@ -5,7 +5,34 @@ import time
 
 
 
-def pwm(frequency):
+def arm_raise(frequency):
+
+    GPIO.setmode(GPIO.BOARD)
+    
+    # pin 13
+
+    GPIO.setup(13, GPIO.OUT)
+
+    # 12 is the pin number of PI
+    # 50 is the frequency
+    p = GPIO.PWM(12, 50)
+
+    p.start(7.5)
+
+
+    #Duty Cycle = length / period
+
+    #length is the iwsth of the puulse, which in this case is .5 or 2.5 ms
+    # period is you know 1/ frequency
+    
+        while (t != 0):
+            t = t - 1
+            p.ChangeDutyCycle(2.5)  # turn towards 0 degree
+            time.sleep(1)  # sleep 1 second
+
+            
+
+def arm_lower(frequency):
 
     GPIO.setmode(GPIO.BOARD)
     
@@ -30,12 +57,7 @@ def pwm(frequency):
             t = t -1
             p.ChangeDutyCycle(12.5)  # turn towards 180 degree
             time.sleep(1)
-        t = 10, 000
-        while (t != 0):
-            t = t - 1
-            p.ChangeDutyCycle(2.5)  # turn towards 0 degree
-            time.sleep(1)  # sleep 1 second
-
+         # sleep 1 second
 #root = Tk()
 #root.wm_title('Servo Control')
 #app = App(root)
